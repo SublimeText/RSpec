4d5d9a1c6f9351f7f9dfdf783b74944081bdf155
Po Chen <pchen@zendesk.com>
2014-11-05 22:51:21 +1100

    Refactor open rspec file command
    It now supports projects with multiple folders

--- OpenRSpecFile.py
+++ OpenRSpecFile.py
@@ -5,44 +5,40 @@ from RSpec import shared
 
 class OpenRspecFileCommand(sublime_plugin.WindowCommand):
 
-	def run(self):
-		if not self.window.active_view():
-			return
+    def run(self):
+        if not self.window.active_view(): return
 
-		self.views = []
-		window = self.window
-		current_file_path = self.window.active_view().file_name()
+        current_file_path = self.window.active_view().file_name()
 
-		if re.search(r"\w+\.rb$", current_file_path):
+        if re.search(r"\w+\.rb$", current_file_path):
+            current_file_name = re.search(r"[/\\]([\w.]+)$", current_file_path).group(1)
+            base_name = re.search(r"(\w+)\.rb$", current_file_name).group(1)
+            base_name = re.sub(r"_spec$", "", base_name)
 
-			current_file = re.search(r"([\w\.]+)$", current_file_path).group(1)
-			base_name = re.search(r"(\w+)\.(\w+)$", current_file).group(1)
-			base_name = re.sub('_spec', '', base_name)
+            target_group = shared.other_group_in_pair(self.window)
 
-			source_matcher = re.compile("[/\\\\]" + base_name + "\.rb$")
-			test_matcher   = re.compile("[/\\\\]" + base_name + "_spec\.rb$")
+            print("Current file: " + current_file_name)
+            if current_file_name.endswith("_spec.rb"):
+                source_matcher = re.compile(r"[/\\]" + base_name + "\.rb$")
+                self.open_project_file(source_matcher, target_group)
+            elif current_file_name.endswith(".rb"):
+                test_matcher = re.compile(r"[/\\]" + base_name + "_spec\.rb$")
+                self.open_project_file(test_matcher, target_group)
+            else:
+                print("Error: current file is not valid for RSpec to switch file")
+        else:
+            print("Error: current file is not a ruby file")
 
-			target_group = shared.other_group_in_pair(window)
+    def open_project_file(self, file_matcher, group):
+        for path, dirs, filenames in self.walk_all_folders():
+            for filename in filter(lambda f: f.endswith(".rb"), filenames):
+                current_file = os.path.join(path, filename)
+                if file_matcher.search(current_file):
+                    file_view = self.window.open_file(os.path.join(path, filename))
+                    self.window.run_command("move_to_group", { "group": group })
+                    return print("Opened: " + filename)
+        print("RSpec: No matching files found")
 
-			print("Current file: " + current_file)
-			if  re.search(re.compile(base_name + "_spec\.rb$"), current_file):
-				self.open_project_file(source_matcher, window, target_group)
-			elif re.search(re.compile(base_name + "\.rb$"), current_file):
-				self.open_project_file(test_matcher, window, target_group)
-			else:
-	 			print("Current file is not valid for RSpec switch file!")
-
-	def open_project_file(self, file_matcher, window, group=-1):
-		for root, dirs, files in os.walk(window.folders()[0]):
-			for f in files:
-				if re.search(r"\.rb$", f):
-					cur_file = os.path.join(root, f)
-					# print("Assessing: " + cur_file)
-					if file_matcher.search(cur_file):
-						file_view = window.open_file(os.path.join(root, f))
-						if group >= 0: # don't set the view unless specified
-							window.run_command('move_to_group', {'group': group})
-						self.views.append(file_view)
-						print("Opened: " + f)
-						return
-		print("No matching files!")
+    def walk_all_folders(self):
+        for folder in self.window.folders():
+            yield from os.walk(folder)

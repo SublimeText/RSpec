535af4e05dc89ead149ffa5e99218fd0b58125b3
Po Chen <pchen@zendesk.com>
2015-01-17 17:30:13 +1100

    Fix and improve new module creation

--- RSpecCreateModule.py
+++ RSpecCreateModule.py
@@ -1,69 +1,90 @@
 import sublime, sublime_plugin, time
 import re
-from RSpec import shared
 
-_patterns = dict((k, re.compile('_*' + v)) for (k, v)
-                    in dict(allcamel=r'(?:[A-Z]+[a-z0-9]*)+$',
-                            trailingcamel=r'[a-z]+(?:[A-Z0-9]*[a-z0-9]*)+$',
-                            underscores=r'(?:[a-z]+_*)+[a-z0-9]+$').items())
+from textwrap import dedent
+from RSpec.shared import other_group_in_pair
 
-_caseTransition = re.compile('([A-Z][a-z]+)')
 
-def translate(name, _from, to):
-    leading_underscores = str()
-    while name[0] == '_':
-        leading_underscores += '_'
-        name = name[1:]
+def snake_case(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-    if _from in ('allcamel', 'trailingcamel'):
-        words = _caseTransition.split(name)
-    else:
-        words = name.split('_')
 
-    words = list(w for w in words if w is not None and 0 < len(w))
+class GotoLineCommand(sublime_plugin.TextCommand):
 
-    camelize = lambda words: ''.join(w[0].upper() + w[1:] for w in words)
+    def run(self, edit, line, column=0):
+        pt = self.view.text_point(line - 1, column)
 
-    v = dict(smushed=lambda: ''.join(words).lower(),
-             allcamel=lambda: camelize(words),
-             trailingcamel=lambda: words[0].lower() + camelize(words[1:]),
-             underscores=lambda: '_'.join(words).lower())[to]()
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(pt))
 
-    return leading_underscores + v
+        self.view.show(pt)
+
+
+class RspecNewModuleCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, name, namespace):
+        class_template = dedent('''
+            class {name}
+
+            end
+        '''.lstrip('\n').rstrip(' \n').format(name=name))
+
+        module_template = dedent('''
+            module {module}
+            {definition}
+            end
+        '''.lstrip('\n').rstrip(' \n'))
+
+        template, level = class_template, len(namespace)
+
+        while namespace:
+            module = namespace.pop()
+            template = module_template.format(module=module, definition=self.indent(template))
+
+        self.view.insert(edit, 0, template)
+        self.view.run_command('goto_line', { 'line': 2 + level, 'column': level * 2 })
+
+    def indent(self, text, space=2):
+        return '\n'.join(' ' * space + line for line in text.split('\n'))
+
+
+class RspecNewSpecCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, name):
+        template = dedent('''
+            require 'spec_helper'
+
+            describe {name} do
+
+            end
+        '''.strip('\n').format(name=name))
+
+        self.view.insert(edit, 0, template)
+        self.view.run_command('goto_line', { 'line': 4 })
 
 
 class RspecCreateModuleCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		# self.view.insert(edit, 0, "Hello, World!")
-		self.window.show_input_panel("Enter module name:", "", self.on_done, None, None)
 
-	def on_done(self, text):
+    def run(self):
+        self.window.show_input_panel("Enter module name:", "", self.on_done, None, None)
 
-		# create the module
-		module = self.window.new_file()
-		module.set_syntax_file('Packages/Ruby/Ruby.tmLanguage')
-		module.set_name(translate(text, 'allcamel', 'underscores') + '.rb')
-		module_template = "\n\
-class " + text + "\n\
-end"
-		edit = module.begin_edit()
-		module.insert(edit, 0, module_template)
-		module.end_edit(edit)
+    def on_done(self, text):
+        if not text: return
 
-		# create the spec
-		spec = self.window.new_file()
-		self.window.run_command('move_to_group', {'group': shared.other_group_in_pair(self.window)})
-		spec.set_syntax_file('Packages/Ruby/Ruby.tmLanguage')
-		spec.set_name(translate(text, 'allcamel', 'underscores') + '_spec.rb')
-		spec_template = "require 'spec_helper'\n\
-require '" + translate(text, 'allcamel', 'underscores') + "'\n\n\
-describe " + text + " do\n\
-\tit \"should do something\"\n\
-end"
-		edit = spec.begin_edit()
-		spec.insert(edit, 0, spec_template)
-		spec.end_edit(edit)
+        *namespace, name = re.split(r'/|::', text.strip(' _/'))
 
-		# try:
-		# except ValueError:
-		#     pass
+        # create the module
+        module = self.window.new_file()
+        module.set_syntax_file('Packages/Ruby/Ruby.tmLanguage')
+        module.set_name(snake_case(name) + '.rb')
+
+        module.run_command('rspec_new_module', { 'name': name, 'namespace': namespace })
+
+        # create the spec
+        spec = self.window.new_file()
+        self.window.run_command('move_to_group', { 'group': other_group_in_pair(self.window) })
+        spec.set_syntax_file('Packages/Ruby/Ruby.tmLanguage')
+        spec.set_name(snake_case(name) + '_spec.rb')
+
+        spec.run_command('rspec_new_spec', { 'name': '::'.join(namespace + [name]) })
